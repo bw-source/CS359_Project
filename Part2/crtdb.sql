8afd8fec19faf7de04edfa33c6454c54974e1812
bw-source <70719664+bw-source@users.noreply.github.com>
2023-03-05 22:37:46 -0700

    Update crtdb.sql
    Updated with constraints

--- Part2/crtdb.sql
+++ Part2/crtdb.sql
@@ -1,11 +1,10 @@
 create table Video(
-    videoCode   int,
-    videoLength int,
+    videoCode   int NOT NULL,
+    videoLength int
     PRIMARY KEY(videoCode)
 );
-
 create table Model(
-    modelNo char(10),
+    modelNO varchar(10) NOT NULL,
     width       numeric(6,2),
     height      numeric(6,2),
     weight      numeric (6,2),
@@ -13,109 +12,98 @@ create table Model(
     screenSize  numeric(6,2),
     PRIMARY KEY(modelNo)
 );
-
-create table Site(
-    siteCode    int, 
-    type        varchar(16),
+create table Site( 
+    siteCode    int NOT NULL, 
+    type        varchar(16), 
     address     varchar(100),
     phone       varchar(16),
+    CONSTRAINT site_type CHECK (type = 'bar'or type ='restaurant')
     PRIMARY KEY(siteCode)
 );
-
 create table DigitalDisplay(
-    serialNo        char(10),
+    serialNo        char(10) NOT NULL,
     schedulerSystem char(10),
     modelNo         char(10),
-    FOREIGN KEY(modelNo) REFERENCES Model(modelNo),
-    PRIMARY KEY(serialNo)
+    CONSTRAINT ss CHECK (schedulerSystem = 'Random'or schedulerSystem ='Smart' or schedulerSystem ='Virtue')
+    PRIMARY KEY(serialNo),
+    FOREIGN KEY(modelNo) REFERENCES Model(modelNo)
 );
-
 create table Client(
-    clientId    int,
+    clientId    int NOT NULL,
     name        varchar(40),
     phone       varchar(16),
     address     varchar(100),
     PRIMARY KEY(clientId)
 );
-
 create table TechnicalSupport(
-    empId   int,
+    empId   int NOT NULL,
     name    varchar(40),
     gender  char(1),
     PRIMARY KEY(empId)
 );
-
 create table Administrator(
     empId   int,
     name    varchar(40),
     gender  char(1),
     PRIMARY KEY(empId)
 );
-
 create table Salesman(
     empId   int,
     name    varchar(40),
     gender  char(1),
     PRIMARY KEY(empId)
 );
-
 create table AirtimePackage(
-    packageId   int,
+    packageId   int NOT NULL,
     class       varchar(16),
     startDate   date,
     lastDate    date,
     frequency   int,
     videoCode   int,
+    CONSTRAINT at_class CHECK (class ='economy'or class='whole day' or class ='golden hours')
     PRIMARY KEY(packageId)
 );
-
 create table AdmWorkHours(
-    empId   int,
-    day     date,
+    empID   int NOT NULL,
+    day     date NOT NULL,
     hours   numeric(4,2),
-    FOREIGN KEY(empId) REFERENCES Administrator(empId),
-    PRIMARY KEY(empId, day)
-);
-
+    PRIMARY KEY(empI,day),
+    FOREIGN KEY(empId) REFERENCES Administrator(empId)
+);  
 create table Broadcasts(
-        videoCode   int,
-        siteCode    int,
+        videoCode   int NOT NULL,
+        siteCode    int NOT NULL,
+        PRIMARY KEY(videoCode,siteCode),
         FOREIGN KEY(videoCode)  REFERENCES Video(videoCode),
-        FOREIGN KEY(siteCode)   REFERENCES Site(siteCode),
-        PRIMARY KEY(videoCode, siteCode)
+        FOREIGN KEY(siteCode)   REFERENCES Site(siteCode)
 );
-
-create table Administers(
-    empId       int,
-    siteCode    int,
+create table Administraters(
+    empId       int NOT NULL, 
+    siteCode    int NOT NULL,
+    PRIMARY KEY(empId,siteCode),
     FOREIGN KEY(empId)      REFERENCES Administrator(empId),
-    FOREIGN KEY(siteCode)   REFERENCES Site(siteCode),
-    PRIMARY KEY(empId, siteCode)
+    FOREIGN KEY(siteCode)   REFERENCES Site(siteCode)
 );
-
-create table Specializes(
-    empId   int,
-    modelNo char(10),
+create table Speacializes(
+    empId   int NOT NULL,
+    modelNo char NOT NULL,
+    PRIMARY KEY(empId,modelNo),
     FOREIGN KEY(empId)      REFERENCES TechnicalSupport(empId),
-    FOREIGN KEY(modelNo)    REFERENCES Model(modelNo),
-    PRIMARY KEY(empId, modelNo)
+    FOREIGN KEY(modelNo)    REFERENCES Model(modelNo)
 );
-
 create table Purchases(
-    clientId    int,
-    empId       int,
-    packageId   int,
-    commissionRate   numeric(4,2),
-    FOREIGN KEY(clientId)   REFERENCES Client(clientId),
+    clientId    int NOT NULL,
+    empId       int NOT NULL,
+    packageId   int NOT NULL,
+    commisionRate   numeric(4,2),
+    CONSTRAINT Pur_trans PRIMARY KEY(clientId,empId,packageId),
     FOREIGN KEY(empId)      REFERENCES Salesman(empId),
-    FOREIGN KEY(packageId)  REFERENCES AirtimePackage(packageId),
-    PRIMARY KEY(clientId, empId, packageId)
+    FOREIGN KEY(packageId)  REFERENCES AirtimePackage(packageId)
 );
-
 create table Locates(
-    serialNo    char(10),
-    siteCode    int,
+    serialNo    char(10) NOT NULL,
+    siteCode    int NOT NULL,
+    CONSTRAINT Loc PRIMARY KEY(serialNo,siteCode),
     FOREIGN KEY(serialNo)   REFERENCES DigitalDisplay(serialNo),
-    FOREIGN KEY(siteCode)   REFERENCES Site(siteCode),
-    PRIMARY KEY(serialNo, siteCode)
+    FOREIGN KEY(siteCode)   REFERENCES Site(siteCode)
 );
