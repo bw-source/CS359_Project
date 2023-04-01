ba5c7a78d49be8a0e6b2f36bc2e627adeee203c8
Thomas Crow <thomas.crow@enmu.edu>
2023-04-01 14:23:15 -0600

    Added creation of Technical Support table

--- Part3/proj.py
+++ Part3/proj.py
@@ -41,11 +41,11 @@ def create_table(conn, create_table_sql):
 def main():
     database = r"proj3db.sqlite"
 
-    sql_create_Video_table = """ CREATE TABLE IF NOT EXISTS Video (
+    sql_create_Video_table =        """ CREATE TABLE IF NOT EXISTS Video (
                                         videoCode       integer PRIMARY KEY NOT NULL,
                                         videoLength     integer NOT NULL
                                     ); """
-    sql_create_Model_table = """ CREATE TABLE IF NOT EXISTS Model (
+    sql_create_Model_table =        """ CREATE TABLE IF NOT EXISTS Model (
                                         modelNO         varchar(10) PRIMARY KEY NOT NULL,
                                         width           numeric(6,2),
                                         height          numeric(6,2),
@@ -53,7 +53,7 @@ def main():
                                         depth           numeric(6,2),
                                         screensize      numeric(6,2)
                                     ); """
-    sql_create_Site_table = """ CREATE TABLE IF NOT EXISTS Site (
+    sql_create_Site_table =         """ CREATE TABLE IF NOT EXISTS Site (
                                         siteCode        integer PRIMARY KEY NOT NULL, 
                                         type            varchar(16), 
                                         address         varchar(100),
@@ -67,7 +67,18 @@ def main():
                                         ss              CHECK (schedulerSystem = 'Random'or schedulerSystem ='Smart' or schedulerSystem ='Virtue'),
                                         FOREIGN KEY     (modelNo) REFERENCES Model (modelNo)
                                     ); """
-    # create a database connection
+    sql_create_Client_table =       """ CREATE TABLE IF NOT EXISTS Client (
+                                        clientId        integer PRIMARY KEY NOT NULL,
+                                        name            varchar(40),
+                                        phone           varchar(16),
+                                        address         varchar(100)
+                                    ); """
+    sql_create_TechnicalSupport_table = """ CREATE TABLE IF NOT EXISTS TechnicalSupport (
+                                        empId   integer NOT NULL,
+                                        name    varchar(40),
+                                        gender  char(1)
+                                    ); """
+# create a database connection
     conn = create_connection(database)
 
     # create tables
@@ -80,6 +91,10 @@ def main():
         create_table(conn,sql_create_Site_table)
         # create DigitalDisplay table
         create_table(conn,sql_create_DigitalDisplay_table)
+        # create Client table
+        create_table(conn,sql_create_Client_table)
+        # create TechnicalSupport table
+        create_table(conn,sql_create_TechnicalSupport_table)
 
     else:
         print("Error! cannot create the database connection.")
