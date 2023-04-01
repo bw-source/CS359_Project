1fd9758a3a32575aeb6b632f93977af3e25c3705
Thomas Crow <thomas.crow@enmu.edu>
2023-04-01 13:47:55 -0600

    Changed name of database created to proj3.sqlite. Added the creation of the Video table

--- Part3/proj.py
+++ Part3/proj.py
@@ -3,20 +3,55 @@ from sqlite3 import Error
 
 
 def create_connection(db_file):
-    """ create a database connection to a SQLite database """
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
+        return conn
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def main():
+    database = r"proj3db.sqlite"
+
+    sql_create_Video_table = """ CREATE TABLE IF NOT EXISTS Video (
+                                        videoCode integer PRIMARY KEY NOT NULL,
+                                        videoLength integer NOT NULL
+                                    ); """
+
+
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create Video table
+        create_table(conn, sql_create_Video_table)
+
+    else:
+        print("Error! cannot create the database connection.")
 
 
 if __name__ == '__main__':
-    create_connection(r"pythonsqlite.db")
-
-def main():
-    print("Test")
+    main()
