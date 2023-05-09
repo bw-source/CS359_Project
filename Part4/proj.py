b1a0ca8842a6abdf4c9550190be8125b7e30d9dd
Thomas Crow <thomas.crow@enmu.edu>
2023-05-08 22:08:37 -0600

    Created submenu and clear screen functions

--- Part4/proj.py
+++ Part4/proj.py
@@ -5,8 +5,28 @@
 #   Notes:
 #********************************************************************************
 
-import sqlite3, os
+import sqlite3
 from sqlite3 import Error
+from os import system, name
+
+#*******************************************************************************
+#   clear_screen()
+#   Purpose: Clear the screen of the OS's terminal
+#
+#   Param: None
+#   Return: None
+#********************************************************************************
+
+# define our clear function
+def clear_screen():
+ 
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+ 
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 
 #*******************************************************************************
@@ -19,7 +39,7 @@ from sqlite3 import Error
 
 def get_db_fileName():
 
-    db_filename = input("What is the name of the SQLite3 database: ")
+    db_filename = input("What is the filename of the Digital Display Database: ")
 
     return db_filename
 
@@ -67,20 +87,52 @@ def userTopLevelMenu():
     userInput = ''
     db_connection = None
 
-    while userInput.upper() != 'Q':
-        print("1.  Login")
-        print("Q.  Quit")
+
+    while (userInput.upper() != 'Q'):
+        clear_screen()
+        print("1.  Log into Digital Display Database System")
+        print("Q.  Quit the program")
         userInput = input("? ")
     
         if userInput == '1':
             db_fileName = get_db_fileName()
             db_connection = create_Connection(db_fileName)
+            if db_connection != None:
+                userSubLevelMenu(db_connection)
+                close_connection(db_connection)
+            else:
+                print("Please enter a valid database filename.")
+                input("Press enter to continue.")
 
         elif userInput.upper() == 'Q':
             continue        
+        else:
+            print("Please enter valid input.")
+            input("Press enter to continue.")
 
-    if db_connection != None:
-        close_connection(db_connection)
+    
+
+#*******************************************************************************
+#   userSubLevelMenu()
+#   Purpose: Create a Sub Level User Menu
+#   Param: None
+#   Return: None
+#********************************************************************************
+
+def userSubLevelMenu(db_conn):
+
+    userInput = ''
+
+    while (userInput.upper() != 'Q'):
+        clear_screen()
+        print("1. Display all the digital displays")
+        print("2. Search digital displays")
+        print("3. Add a new digital display")
+        print("4. Edit an exisiting digital display")
+        print("5. Delete and existing digital display") 
+        print("Q. Quit and log out of current database")
+
+        userInput = input("? ")
 
 
 def main():
